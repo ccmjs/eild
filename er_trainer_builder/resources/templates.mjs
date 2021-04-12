8c21454ff4dd7f46db0f85d094cf61607363089e
akless <andre.kless@web.de>
2021-04-12 22:22:34 +0200

    updated ccm.er_trainer_builder.js: preview with larger modal dialog

--- er_trainer_builder/resources/templates.mjs
+++ er_trainer_builder/resources/templates.mjs
@@ -1031,7 +1031,7 @@ export function main( config, builder, onDeleteNotation, onResetNotations, onDel
     
     <!-- Modal: Preview -->
     <div class="modal fade" id="erb-preview" tabindex="-1" aria-labelledby="App Preview" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-scrollable">
+      <div class="modal-dialog modal-lg modal-dialog-scrollable">
         <div class="modal-content">
   
           <!-- Modal Header -->
