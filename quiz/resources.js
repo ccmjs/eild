4c81e1c4918283a36a27a5321c80dc52c96e40d6
akless <andre.kless@web.de>
2021-04-15 10:48:35 +0200

    added files for multiple choice quiz

---
+++ quiz/resources.js
@@ -0,0 +1,87 @@
+/**
+ * @overview data-based resources for quiz
+ * @author André Kless <andre.kless@web.de> 2021
+ * @license The MIT License (MIT)
+ */
+
+ccm.files[ 'resources.js' ] = {
+
+  "db_ss21_eh02": {
+    "css": [ "ccm.load", "https://ccmjs.github.io/akless-components/quiz/resources/weblysleek.css", { "context": "head", "url": "https://ccmjs.github.io/akless-components/libs/weblysleekui/font.css" } ],
+    "data": {
+      "key": "eh20",
+      "store": [
+        "ccm.store",
+        {
+          "name": "db-ss21-quiz-results",
+          "url": "https://ccm2.inf.h-brs.de"
+        }
+      ]
+    },
+    "onfinish": {
+      "confirm": "",
+      "clear": false,
+      "restart": true,
+      "alert": ""
+    },
+    "questions": [
+      {
+        "text": "Gegeben sind die folgenden beiden Datensätze der Datensammlung \"Personen\":",
+        "input": "checkbox",
+        "description": "\"Harald Kaputnik, Hauptstraße 18, 53117 Bonn\" und\n\"Petra Weizenkeim, 22.06.1999, 0228-12345678, 53117 Bonn\"",
+        "answers": [
+          {
+            "text": "Die Datensammlung \"Personen\" vereinigt unstrukturierte Daten.",
+            "correct": true,
+            "comment": "Weil das richtig ist."
+          },
+          {
+            "text": "Die Datensammlung \"Personen\" vereinigt semi-strukturierte Daten.",
+            "correct": false,
+            "comment": "Weil das leider falsch ist."
+          },
+          {
+            "text": "Die Datensammlung \"Personen\" vereinigt strukturierte Daten.",
+            "correct": false,
+            "comment": ""
+          },
+          {
+            "text": "Die Bedeutung der Datenwerte ist für den Leser nicht ersichtlich.",
+            "correct": true,
+            "comment": ""
+          },
+          {
+            "text": "Es könnte sich um zwei Datensätze einer Tabelle \"Personen\" handeln.",
+            "correct": false,
+            "comment": ""
+          }
+        ]
+      }
+    ],
+    "start_button": false,
+    "feedback": true,
+    "progress_bar": false,
+    "navigation": false,
+    "skippable": false,
+    "anytime_finish": false,
+    "time": 180,
+    "shuffle": false,
+    "random": true,
+    "escape": false,
+    "user": [
+      "ccm.instance",
+      "https://ccmjs.github.io/akless-components/user/versions/ccm.user-9.7.1.js",
+      {
+        "realm": "guest",
+        "guest": true
+      }
+    ],
+    "placeholder": {
+      "prev": "Vorherige Frage",
+      "submit": "Abschicken",
+      "next": "Nächste Frage",
+      "finish": "Speichern"
+    }
+  }
+
+};
